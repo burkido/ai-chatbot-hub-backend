caf0a010bf8c95c3872ae37024dbae53cc528b93
burak <karadumanburak000@gmail.com>
2025-02-27 15:49:00 +0300

    Remove debug print statements from superuser token header utility function

--- backend/app/tests/utils/utils.py
+++ backend/app/tests/utils/utils.py
@@ -15,16 +15,12 @@ def random_email() -> str:
 
 
 def get_superuser_token_headers(client: TestClient) -> dict[str, str]:
-    print("FIRST_SUPERUSER", settings.FIRST_SUPERUSER)
-    print("FIRST_SUPERUSER_PASSWORD", settings.FIRST_SUPERUSER_PASSWORD)
     login_data = {
         "username": settings.FIRST_SUPERUSER,
         "password": settings.FIRST_SUPERUSER_PASSWORD,
     }
     r = client.post(f"{settings.API_V1_STR}/auth/login", data=login_data)
     tokens = r.json()
-    print("auth utils", tokens)
     a_token = tokens["access_token"]
     headers = {"Authorization": f"Bearer {a_token}"}
-    print("auth utils", headers)
     return headers
