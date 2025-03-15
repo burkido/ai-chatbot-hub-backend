5bc82c5dadfdd5cc39b084e363581b42f009ad75
burak <karadumanburak000@gmail.com>
2025-03-15 16:34:25 +0300

    Update environment configuration for local development and enhance email template structure

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -83,7 +83,7 @@ def generate_test_email(email_to: str) -> EmailData:
 def generate_reset_password_email(email_to: str, email: str, token: str) -> EmailData:
     project_name = settings.PROJECT_NAME
     subject = f"{project_name} - Password recovery for user {email}"
-    link = f"{settings.server_host}/reset-password?token={token}"
+    link = f"https://assistlyai.space/reset-password?token={token}"
     html_content = render_email_template(
         template_name="reset_password.html",
         context={
