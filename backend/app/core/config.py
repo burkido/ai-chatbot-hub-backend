8d10279390f138eaf8dc800106ddee7cffe85496
burak <karadumanburak000@gmail.com>
2025-04-06 23:57:29 +0300

    Restore ACCESS_TOKEN_EXPIRE_MINUTES to 5 minutes for consistency in token expiration

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -29,8 +29,7 @@ class Settings(BaseSettings):
     )
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    #ACCESS_TOKEN_EXPIRE_MINUTES: int = 5            # 5 minutes = 5 minutes
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 1
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 5            # 5 minutes = 5 minutes
     REFRESH_TOKEN_EXPIRE_DAYS: int = 60 * 24 * 8    # 60 minutes * 24 hours * 8 days = 8 days
     DOMAIN: str = "localhost"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
